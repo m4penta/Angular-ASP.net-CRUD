9d0674bf6f9dcfb40ff15356c5f12c27dd24e069
m4penta <m5pentagenx@gmail.com>
2023-09-21 21:27:03 +0700

    DEFINE EF Model & DB Context Class -DB Migration

--- PaymentAPI/Models/PaymentDetail.cs
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -1,6 +1,23 @@
-﻿namespace PaymentAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
 {
     public class PaymentDetail
     {
+        [Key] //Chú thích này xác định trường PaymentDetailId là trường khóa chính
+        public int PaymentDetailId { get; set; }
+        //Chú thích này xác định định dạng và kích thước dữ liệu 
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
     }
 }
--- PaymentAPI/Program.cs
+++ PaymentAPI/Program.cs
@@ -1,14 +1,24 @@
+using PaymentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PaymentDetailContext>
+(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseStaticFiles();
 
